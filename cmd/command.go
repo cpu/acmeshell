3ce51c0d477c2954879f878b8b889d565c02410d
Daniel <daniel@binaryparadox.net>
2018-10-20 13:15:23 -0400

    Add standalone acmeshell-challtestsrv command

--- cmd/command.go
+++ cmd/command.go
@@ -2,6 +2,9 @@ package cmd
 
 import (
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func FailOnError(err error, msg string) {
@@ -12,4 +15,29 @@ func FailOnError(err error, msg string) {
 
 	// Otherwise, print the error and fail
 	log.Fatalf("[!] %s - %s", msg, err)
+}
+
+var signalToName = map[os.Signal]string{
+	syscall.SIGTERM: "SIGTERM",
+	syscall.SIGINT:  "SIGINT",
+	syscall.SIGHUP:  "SIGHUP",
+}
+
+// CatchSignals catches SIGTERM, SIGINT, SIGHUP and executes a callback
+// method before exiting
+func CatchSignals(callback func()) {
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGTERM)
+	signal.Notify(sigChan, syscall.SIGINT)
+	signal.Notify(sigChan, syscall.SIGHUP)
+
+	sig := <-sigChan
+	log.Printf("Caught %s", signalToName[sig])
+
+	if callback != nil {
+		callback()
+	}
+
+	log.Printf("Exiting")
+	os.Exit(0)
 }
